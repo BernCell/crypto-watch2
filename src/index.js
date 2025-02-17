f93fedde31151682dfce86e6246d4e1358e88395
BernCell <bern.cell@hotmail.com>
2025-02-17 23:50:05 +0100

    add redux devtools et dépendances

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Modifie l'importation
 import App from "./App";
 import "./styles/index.scss";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// REDUX
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "./reducer";
+
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: true,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
